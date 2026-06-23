b6474d9480bc099f9fe5c56b83c3c32644422459
MSHI <archie8teen@gmail.com>
2026-06-24 07:50:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Temple Of God Loaded");
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+const icon = menuToggle.querySelector("i");
+
+// TOGGLE MENU
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    if(navLinks.classList.contains("active")){
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+    } else {
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+    }
+});
+
+// CLOSE MENU WHEN CLICK LINK
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+    });
+});
+
+// DROPDOWN CLICK (MOBILE)
+const dropdown = document.querySelector(".dropdown");
+
+dropdown.addEventListener("click", (e) => {
+    if(window.innerWidth <= 768){
+        e.preventDefault();
+        dropdown.classList.toggle("active");
+    }
+});
